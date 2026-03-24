1a538d07100d0e57774fccc93f00b9450fc2bc5f
ADARSH0309 <adarskr03@gmail.com>
2026-03-24 13:28:22 +0530

    Update hooks barrel export with all hooks

--- frontend/client/src/hooks/index.ts
+++ frontend/client/src/hooks/index.ts
@@ -1,3 +1,5 @@
 export { useKeyboardShortcut } from './useKeyboardShortcut';
 export { useLocalStorage } from './useLocalStorage';
 export { useDebounce } from './useDebounce';
+export { useOnlineStatus } from './useOnlineStatus';
+export { useMediaQuery, useIsMobile } from './useMediaQuery';
